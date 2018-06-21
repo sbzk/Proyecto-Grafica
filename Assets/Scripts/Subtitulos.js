636502420dc29cf1d1935ca7112740ca627905d7
Seba Benamo <seba.benamo@gmail.com>
2018-06-21 14:17:11 -0300

    Subtitulos y pastos

---
+++ Assets/Scripts/Subtitulos.js
@@ -0,0 +1,47 @@
+﻿var canvas : GameObject;
+private var yapase : boolean;
+private var xavi : boolean;
+private var pasehacepoco : boolean;
+var SubAlarma : UnityEngine.UI.Text;
+var SubEncontrar : UnityEngine.UI.Text;
+var SubRepasar : UnityEngine.UI.Text;
+private var objetosPorEncontrar : int;
+
+function Start () {
+	yapase=false;
+	xavi=false;
+	pasehacepoco=false;
+}
+
+
+function OnTriggerStay (other : Collider){	
+	if(other.tag == "SubAlarma"){		
+		SubAlarma.enabled=true;
+		yield WaitForSeconds(4f);
+		SubAlarma.enabled=false;
+		other.enabled=false;	
+	}
+	if(other.tag == "SubNave"){		
+		if(!yapase){
+			SubEncontrar.enabled=true;
+			yield WaitForSeconds(4f);
+			SubEncontrar.enabled=false;
+			yapase=true;
+			yield WaitForSeconds(10f);
+			xavi=true;
+			
+		}
+		else{
+			if (GameObject.FindGameObjectsWithTag("Recolectable").length!=0 && xavi && !pasehacepoco){
+				SubRepasar.enabled=true;
+				yield WaitForSeconds(4f);
+				SubRepasar.enabled=false;
+				pasehacepoco=true;
+				yield WaitForSeconds(15f);
+				pasehacepoco=false;
+			}
+		}
+	}
+	
+	
+}
